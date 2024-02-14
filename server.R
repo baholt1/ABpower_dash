d0b1823d0ae921b6557caa8dcebbe1f0272ece3e
baholt1 <baholt1@ualberta.ca>
2024-02-13 17:03:42 -0700

    Add lng lat jitter.

--- server.R
+++ server.R
@@ -16,13 +16,16 @@ server <- function(input, output) {
                        Master_Location, 
                        by.x = "name", 
                        by.y = "Area Name")
+    
+    locations$jittered_lng <- jitter(locations$long)
+    locations$jittered_lat <- jitter(locations$lat)
 
     
     map <- leaflet() %>%
       addTiles() %>%
       addCircleMarkers(data = locations, 
-                       lng = ~long, 
-                       lat = ~lat, 
+                       lng = ~jittered_lng, 
+                       lat = ~jittered_lat, 
                        popup = ~locations$`Facility Name`,
                        radius = 5,
                        color = "red")
