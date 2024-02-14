f992ed2de82bff6f488b545a382530d9e1b34036
baholt1 <baholt1@ualberta.ca>
2024-02-13 17:25:59 -0700

    Add boundaries

--- server.R
+++ server.R
@@ -1,12 +1,19 @@
 library(shiny)
 library(maps)
 library(ggplot2)
+library(sf)
 
 # Define server logic
 server <- function(input, output) {
   
   # Function to create Alberta map
   output$alberta_map <- renderLeaflet({
+    path <- "C:/Users/holtb/OneDrive/Documents/UAlberta 2023-2024 Year 4/Winter 2024/FIN 488 FINTECH3/ABpower_dash/AESO-Planning-Areas-2020-06-23"
+    
+    boundaries <- sf::st_read(dsn = path)
+    
+    bound_cols <- rainbow(nrow(boundaries))
+    
     ab.city <- canada.cities %>% 
       dplyr::filter(country.etc == "AB") %>% 
       dplyr::mutate(name = stringr::str_replace(name, 
@@ -22,13 +29,20 @@ server <- function(input, output) {
 
     
     map <- leaflet() %>%
-      addTiles() %>%
-      addCircleMarkers(data = locations, 
-                       lng = ~jittered_lng, 
-                       lat = ~jittered_lat, 
-                       popup = ~locations$`Facility Name`,
-                       radius = 5,
-                       color = "red")
+        addTiles() %>%
+        addCircleMarkers(data = locations,
+                         lng = ~jittered_lng,
+                         lat = ~jittered_lat,
+                         popup =  ~locations$`Facility Name`,
+                         radius = 5,
+                         color = "red") %>%
+        addPolygons(data = boundaries,
+                    fillColor = bound_cols,
+                    fillOpacity = 0.1,
+                    color = "black",
+                    stroke = T,
+                    weight = 1,
+                    popup = ~boundaries$NAME)
     
     return(map)
   })
