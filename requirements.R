be3bcc24555f72a3c02e95d7af27567c1931b8ec
baholt1 <baholt1@ualberta.ca>
2024-02-15 09:03:56 -0700

    Chnage reqs

--- requirements.R
+++ requirements.R
@@ -1,1 +1,6 @@
-install.packages(c("tidyverse", "plotly", "shiny", "maps", "sf", "rvest", "leaflet"))
+pack <- c("tidyverse", "plotly", "shiny", "maps", "sf", "rvest", "leaflet")
+
+new.packages <- pack[!(pack %in% installed.packages()[, "Package"])]
+if (length(new.packages)) {
+  install.packages(new.packages, dependencies = TRUE)
+}
