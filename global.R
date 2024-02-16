98d2ee42a1eedc8c3294d0b5bfe22d08eb2e62bb
Olivier Haley <129203990+tigerwoodsjr@users.noreply.github.com>
2024-02-16 00:56:02 -0700

    final but actually

--- global.R
+++ global.R
@@ -9,48 +9,19 @@ library(grDevices)
 
 
 boundaries_1 <- st_read("AESO-Planning-Areas-2020-06-23/AESO_Planning_Areas.shp") 
-
+# Make sf object
 boundaries <- sf::st_as_sf(boundaries_1, coords = c("long", "lat"), crs = "+proj=longlat +datum=NAD83 +no_defs") %>% 
   sf::st_transform( crs = "+proj=longlat +datum=WGS84") %>% 
   mutate(Area_ID = as.numeric(Area_ID)) 
 
 
-#gen file with comprehensive area-asset mapping 
+# gen file with comprehensive area-asset mapping 
 genFile <- read_csv("CSD Generation (Hourly) - 2024-01 2.csv") %>% 
   dplyr::transmute(assetID = `Asset Short Name`, Area_ID = `Planning Area`, region = Region) %>% 
   dplyr::group_by(assetID) %>% 
   dplyr::slice(1)
 
-
-Master_Location <- read_excel("Master-Location.xlsx")
-
-colNames <- Master_Location %>% 
-  dplyr::slice(6) %>%
-  unlist(use.names = FALSE)
-
-colnames(Master_Location) <- colNames
-
-Master_Location <- Master_Location %>% 
-  dplyr::slice(-1:-6)
-
-
-bound_cols <- grDevices::rainbow(nrow(boundaries))
-
-
-ab.city <- maps::canada.cities %>% 
-  dplyr::filter(country.etc == "AB") %>% 
-  dplyr::mutate(name = stringr::str_replace(name, 
-                                            pattern = " AB", 
-                                            replacement = ""))
-locations <- merge(ab.city, 
-                   Master_Location, 
-                   by.x = "name", 
-                   by.y = "Area Name")
-
-locations$jittered_lng <- jitter(locations$long)
-locations$jittered_lat <- jitter(locations$lat)
-
-#function is pretty crummu, will rewrite a better one at some point
+# Scrape function
 scrapeGen <- function(aesoDashSource ){
   
   AESOsimpleTable <- function(table, subtype) {
@@ -99,7 +70,7 @@ scrapeGen <- function(aesoDashSource ){
   # taking existing col names for type column
   type <- colnames(gas)
   
-  # overwritting col names with def list
+  # overwriting col names with def list
   colnames(gas) <- colNames
   
   
@@ -122,7 +93,7 @@ scrapeGen <- function(aesoDashSource ){
       ASSET %in% c("Simple Cycle", "Cogeneration", "Combined Cycle", "Gas Fired Steam") ~ ASSET)) %>% 
     # fill remainder of subtype until new subtype
     tidyr::fill(subtype, .direction = c("down")) %>% 
-    # removing divsional rows (now that they are in column subtype)
+    # removing divisional rows (now that they are in column subtype)
     dplyr::filter(!ASSET %in% 
              c("Simple Cycle", "Cogeneration", "Combined Cycle", "Gas Fired Steam", "ASSET")) %>% 
     dplyr::mutate(date = dateMST)
